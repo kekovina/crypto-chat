c22a75d45e63776495bfe9ec67ed3ee9803389ba
Anton Azarov <kekovinya@gmail.com>
2025-05-06 18:36:12 +0300

    fix: socket singletone

--- src/shared/hooks/useSocket.ts
+++ src/shared/hooks/useSocket.ts
@@ -1,58 +1,47 @@
+// useSocket.ts
+import { getSocket } from '@/shared/libs/getSocket';
 import { useCallback, useEffect, useRef } from 'react';
-import { io, ManagerOptions, Socket, SocketOptions } from 'socket.io-client';
+import { ManagerOptions, Socket, SocketOptions } from 'socket.io-client';
 
 type DefaultEventsMap = Record<string | symbol, (...args: any[]) => void>;
 
 interface UseSocketOptions extends Partial<ManagerOptions & SocketOptions> {
-  url?: string; // По умолчанию будет текущий хост
+  url?: string;
 }
 
 export function useSocket<
   ServerEvents extends DefaultEventsMap = DefaultEventsMap,
   ClientEvents extends DefaultEventsMap = DefaultEventsMap,
 >(options: UseSocketOptions = {}) {
-  const socketRef = useRef<Socket<ServerEvents, ClientEvents> | null>(null);
+  const socketRef = useRef<Socket<ServerEvents, ClientEvents>>(null);
+
+  if (!socketRef.current) {
+    socketRef.current = getSocket(options.url ?? '/', options);
+  }
 
   useEffect(() => {
-    const {
-      url,
-      path = '/api/socket',
-      autoConnect = true,
-      transports = ['websocket'],
-      ...rest
-    } = options;
-
-    const socket: Socket<ServerEvents, ClientEvents> = io(url ?? undefined, {
-      path,
-      autoConnect,
-      transports,
-      ...rest,
-    });
-
-    socketRef.current = socket;
-
     return () => {
-      socket.disconnect();
-      socketRef.current = null;
+      socketRef.current?.removeAllListeners(); // не disconnect, если хочешь singleton
     };
-  }, [options]);
-
-  const emit = useCallback<
-    <K extends keyof ServerEvents>(event: K, ...args: Parameters<ServerEvents[K]>) => void
-  >((event, ...args) => {
-    socketRef.current?.emit(event, ...args);
   }, []);
 
+  const emit = useCallback(
+    <K extends keyof ServerEvents>(event: K, ...args: Parameters<ServerEvents[K]>) => {
+      socketRef.current?.emit(event as string, ...args);
+    },
+    []
+  );
+
   const on = <K extends keyof ClientEvents>(event: K, callback: ClientEvents[K]) => {
-    socketRef.current?.on(event, callback);
+    socketRef.current?.on(event as string, callback);
   };
 
   const off = <K extends keyof ClientEvents>(event: K, callback: ClientEvents[K]) => {
-    socketRef.current?.off(event, callback);
+    socketRef.current?.off(event as string, callback);
   };
 
   return {
-    socket: socketRef.current,
+    socket: socketRef.current!,
     emit,
     on,
     off,
